37bfb62d824b2a0002f9b9948a6c4eeedd3ba3ca
stephenfreund <freund@cs.williams.edu>
2025-04-03 06:44:26 -0400

    accounts/keys

--- README.md
+++ README.md
@@ -3,32 +3,30 @@
 # Flowco
 ![flowco](https://github.com/user-attachments/assets/9591f546-ef52-4c16-999a-ca9e2a149586)
 
-Flowco needs a better description.
-
 ## Web
 
 [https://go-flow.co](https://go-flow.co)
 
-Log in with Google credentials.  All files are stored on AWS S3 partition.  This uses my own OpenAI key for now...
-
-## Local
+## Local Install
 
 ### Conda or other virtual env
 
-Definitely use a conda virtual environment or some other virtual environment, and Python 3.11.  
-Should work with other versions, but that matches the web service environtment.
+* Definitely use a conda virtual environment or some other virtual environment.
+* Use Python 3.11.  
 
-### Installation
+### API Key
 
 > [!IMPORTANT]
 >
-> Flowco currently needs to be connected to an [OpenAI account](https://openai.com/api/). _Your account will need to have a positive balance for this to work_ ([check your balance](https://platform.openai.com/account/usage)). If you have never purchased credits, you will need to purchase at least \$1 in credits (if your API account was created before August 13, 2023) or \$0.50 (if you have a newer API account) in order to have access to GPT-4, which Flowco uses. [Get a key here.](https://platform.openai.com/account/api-keys)
+> Flowco currently needs to be connected to an [OpenAI account](https://openai.com/api/). _Your account will need to have a positive balance for this to work_ ([check your balance](https://platform.openai.com/account/usage)). [Get a key here.](https://platform.openai.com/account/api-keys)
 >
 > Once you have an API key, set it as an environment variable called `OPENAI_API_KEY`.
 >
 > ```bash
 > export OPENAI_API_KEY=<your-api-key>
 > ```
+
+### Building
 
 Clone the repo and install with `make`: 
 
@@ -38,102 +36,12 @@ make
 
 This installs a bunch of normal packages through a hatchling `pyproject.toml`, and then also a local wheel for the MXGraph component.
 
-### Command line:
+### Running
 
-* Make a copy of the `test` directory so you don't inadvertently 
-  commit changes to the examples.
-    ```bash
-    cp -r test /tmp/test
-    ```
-
-* Run `flowco` from the directory containing the `.flowco` file:
-    ```bash
-    cd /tmp/test/finch
-    flowco finch-small.flowco build run
-    ```
-
-* View the output:
-    ```bash
-    flowco finch-small.flowco html
-    ```
-
-* Reset the file to clear out all generated node components:
-    ```bash
-    flowco finch-small.flowco reset
-    ```
-
-### GUI
-
-* From the root of the flowco repository, run this command with the directory if the flowco files you want to use:
-    ```bash
-    flowco-ui /tmp/test/finch
-    ```
-
-    You will get a few new demo files in that directory.  They have some simple how-to instructions that may help you
-    get started.
-
-
-### Flowthon: For those who like source code.
-
-* Make the source code for an existing file:
-    ```bash
-    flowco finch-small.flowco export
-    ```
-
-    Or a new one:
-        ```bash
-        flowthon create test.flowthon
-        ```
-
-* Edit the flowthon file in your favorite editor.
-
-* Run it:
-
-    ```bash
-    flowthon run finch-small.flowthon
-    ```
-
-    **Note:** This will modify your flowthon file in place to keep it in sync with the dataflow graph.  Be sure to reload it before editing.
-
-* Base structure of file:
-
-    ```
-    def create_1000():
-        """
-        Label
-
-        # Requirements
-        - ...
-
-        # Algorithm
-        - ...
-
-        # Assertions
-        - ...
-        """
-        ...
-
-    def plot_purple(create_1000):
-        """
-        ...
-        """
-        ...
-    ```
-
-    The `...` for the code is necessary.  The other parts can be edited/omitted however you want.
-
-* To see all parts in the flowthon file, rerun `export` as follows.
-
-    ```bash
-    flowco finch-small.flowco export --code
-    ```
-
-
-## "Sketchy" Behavior or Bugs?
-
-If you see a bug -- hahaha, **when** you see a bug -- please send me the Flowco file and the `logging.txt` file from the working directory, or upload to a github issue...
-
-## Known Issues
-
-* So many I can't even begin to list them...
+On the command line, run `flowco`, passing it the directory in which to store its files.  That directory
+should already exist:
+```bash
+mkdir /tmp/example
+flowco /tmp/example
+```
 
---
+++ src/flowco/assistant/flowco_keys.py
@@ -0,0 +1,154 @@
+from datetime import datetime, timedelta
+import os
+from typing import Dict, List
+
+from pydantic import BaseModel
+
+from flowco.session.session import session
+from flowco.session.session_file_system import fs_read, fs_write
+from flowco.util.output import log
+from llm.assistant import AssistantError
+
+
+class FlowcoKeys:
+    def get_api_key(self, api_key_name: str) -> str | None:
+        pass
+
+    def set_api_key(self, key_name: str, key_value: str):
+        pass
+
+    def get_api_key_status(self, api_key_name: str) -> str:
+        pass
+
+
+class KeyEnv(FlowcoKeys):
+    def get_api_key(self, api_key_name: str) -> str | None:
+        key = os.environ.get(api_key_name, None)
+        if key is None:
+            key = os.environ.get("DEFAULT_" + api_key_name, None)
+        return key
+
+    def set_api_key(self, key_name: str, key_value: str):
+        os.environ[key_name] = key_value
+
+    def get_api_key_status(self, api_key_name: str) -> str:
+        key = self.get_api_key(api_key_name)
+        if key is None:
+            return "Key {api_key_name} is not in the environment variables."
+        else:
+            return f"Key {api_key_name} is set."
+
+
+class ApiKey(BaseModel):
+    key: str
+    expiration: datetime
+
+
+key_file_name = "keys.json"
+
+
+class KeyFile(BaseModel):
+    keys: Dict[str, ApiKey]
+
+    @staticmethod
+    def load() -> "KeyFile":
+        try:
+            keys_json = fs_read(key_file_name)
+            return KeyFile.model_validate_json(keys_json)
+        except FileNotFoundError:
+            keys = KeyFile._from_defaults()
+            keys.save()
+            return keys
+
+    def save(self):
+        fs_write(key_file_name, self.model_dump_json(indent=2))
+
+    @staticmethod
+    def _from_defaults() -> "KeyFile":
+        openai_key = ApiKey(
+            key=os.environ.get("DEFAULT_OPENAI_API_KEY", ""),
+            expiration=datetime.now() + timedelta(hours=0.5),
+        )
+        # anthropic_key = ApiKey(
+        #     key=os.environ.get("DEFAULT_ANTHROPIC_API_KEY", ""),
+        #     expiration=datetime.now() + timedelta(hours=0.1),
+        # )
+        return KeyFile(
+            keys={
+                "OPENAI_API_KEY": openai_key,
+                # "ANTHROPIC_API_KEY": anthropic_key,
+            }
+        )
+
+    def get_api_key(self, api_key_name: str) -> str | None:
+        key = self.keys.get(api_key_name)
+        if key is not None:
+            if key.expiration > datetime.now():
+                return key.key
+            else:
+                del self.keys[api_key_name]
+                self.save()
+        return None
+
+    def set_api_key(self, key_name: str, key_value: str):
+        self.keys[key_name] = ApiKey(
+            key=key_value,
+            expiration=datetime.now() + timedelta(weeks=52 * 10),
+        )
+        self.save()
+
+    def get_api_key_status(self, api_key_name: str) -> str:
+        key = self.get_api_key(api_key_name)
+        if key is None:
+            return f"Key {api_key_name} has not been set."
+
+        if self.keys[api_key_name].expiration < datetime.now():
+            return f"Key {api_key_name} has expired."
+
+        minutes_to_expire = (
+            self.keys[api_key_name].expiration - datetime.now()
+        ).total_seconds() / 60
+        if minutes_to_expire < 60:
+            return f"Key {api_key_name} will expire in {minutes_to_expire:.1f} minutes."
+
+        return f"Key {api_key_name} is set."
+
+
+class UserKeys(FlowcoKeys):
+
+    def __init__(self):
+        self.key_file = None
+
+    def load(self) -> KeyFile:
+        log("Loading keys")
+        if self.key_file is None:
+            self.key_file = KeyFile.load()
+        return self.key_file
+
+    def get_api_key(self, api_key_name: str) -> str | None:
+        log("Getting key", api_key_name)
+        return self.load().get_api_key(api_key_name)
+
+    def set_api_key(self, key_name: str, key_value: str):
+        return self.load().set_api_key(key_name, key_value)
+
+    def get_api_key_status(self, api_key_name: str) -> str:
+        return self.load().get_api_key_status(api_key_name)
+
+
+def get_api_key(api_key_name: str) -> str:
+    key = session.get("keys", FlowcoKeys).get_api_key(api_key_name)
+    if key is None:
+        raise AssistantError(
+            f"Your API key {api_key_name} is invalid, expired, or revoked.  Add a valid API key under Settings.  For OpenAI, you can find your API key at https://platform.openai.com/account/api-keys."
+        )
+    log(f"Using {api_key_name} key {key}")
+    return key
+
+
+def set_api_key(key_name: str, key_value: str):
+    session.get("keys", FlowcoKeys).set_api_key(key_name, key_value)
+
+
+def get_api_key_status(api_key_name: str) -> str:
+    return session.get("keys", FlowcoKeys).get_api_key_status(api_key_name)
