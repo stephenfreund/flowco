300fcea1fb9104f29be1b2bdd7bc7d9392f7d35d
stephenfreund <freund@cs.williams.edu>
2025-04-09 15:37:57 -0400

    another attempt

--- hatch_build.py
+++ hatch_build.py
@@ -6,6 +6,7 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 class Hook(BuildHookInterface):
     def initialize(self, version, build_data):
+        pkg = "src/mxgraph_component"
         frontend = "src/mxgraph_component/mxgraph_component/frontend"
         subprocess.run(["npm", "install"], cwd=frontend, check=True)
         subprocess.run(["npm", "run", "build"], cwd=frontend, check=True)
