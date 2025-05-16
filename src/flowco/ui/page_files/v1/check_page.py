a4633e429e91e77f0a55ce9e69307d1c7e2ad1b7
stephenfreund <freund@cs.williams.edu>
2025-05-16 07:05:02 -0400

    layout

---
+++ src/flowco/ui/page_files/v1/check_page.py
@@ -0,0 +1,321 @@
+import textwrap
+from typing import List
+import pandas as pd
+import streamlit as st
+from flowco.builder.assertions import suggest_assertions
+from flowco.builder.build import BuildEngine
+from flowco.dataflow.dfg import Node, NodeMessage
+from flowco.dataflow.phase import Phase
+from flowco.ui.page_files.v1.build_page import BuildButton, BuildPage
+from flowco.ui.ui_builder import Builder
+from flowco.ui.ui_page import UIPage
+from flowco.ui.ui_util import (
+    set_session_state,
+    show_code,
+)
+
+
+import streamlit as st
+
+
+from flowco import __main__
+from flowco.ui.ui_page import UIPage
+from flowco.llm.assistant import AssistantError
+
+
+class CheckPage(BuildPage):
+
+    # Override for other pages
+
+    def update_button(self) -> BuildButton:
+        return BuildButton(
+            label="Update",
+            icon=":material/refresh:",
+            action="Update",
+            # passes_key="repair-checks-passes",
+        )
+
+    def run_button(self) -> BuildButton:
+        return BuildButton(
+            label="Check",
+            icon=":material/play_circle:",
+            action="Run",
+            # passes_key="repair-checks-passes",
+        )
+
+    def fix_button(self) -> BuildButton:
+        return BuildButton(
+            label="Fix",
+            icon=":material/build:",
+            action="Run",
+            passes_key="repair-checks-passes",
+        )
+
+    def suggest_button(self) -> BuildButton:
+        return BuildButton(
+            label="Suggest Checks",
+            icon=":material/format_list_bulleted:",
+            action="Run",
+            passes_key="suggest-assertions-passes",
+        )
+
+    def build_target_phase(self) -> Phase:
+        return Phase.assertions_checked
+
+    def messages_triggering_global_error_check(self):
+        node_id = st.session_state.node_id_with_fix_button
+        if node_id is None:
+            return []
+        ui_page: UIPage = st.session_state.ui_page
+        node = ui_page.dfg()[node_id]
+        error_run_messages = node.filter_messages(
+            [x for x in Phase],  #  if x.value <= Phase.run_checked.value],
+            level="error",
+        )
+        return [(node, m) for m in error_run_messages]
+
+    def node_header(self, node: Node):
+        super().node_header(node)
+        assertion_failures = node.filter_messages(Phase.assertions_checked, "error")
+        if assertion_failures:
+            fix_button = self.fix_button()
+
+            def fire():
+                set_session_state("trigger_build_toggle", fix_button)
+                st.session_state.node_id_with_fix_button = node.id
+
+            st.button(
+                fix_button.label,
+                on_click=fire,
+                disabled=not self.graph_is_editable(),
+                help="Fix any errors in the checks",
+            )
+        else:
+            if node.phase >= Phase.assertions_checked:
+                st.success("All checks passed")
+
+    def edit_checks(self, node_id: str):
+        node = st.session_state.tmp_dfg[node_id]
+
+        @st.dialog(f"Output Checks for {node.pill}", width="large")
+        def for_real(node_id: str):
+            top = st.empty()
+            try:
+
+                def make_suggestions():
+                    st.session_state.make_suggestions = True
+
+                node = st.session_state.tmp_dfg[node_id]
+                buttons = st.empty()
+
+                if st.session_state.make_suggestions:
+                    with st.spinner("Making suggestions..."):
+                        st.session_state.make_suggestions = False
+                        suggested_assertions = suggest_assertions(
+                            st.session_state.tmp_dfg, st.session_state.tmp_dfg[node_id]
+                        )
+                        st.session_state.tmp_assertions = (
+                            st.session_state.tmp_assertions + suggested_assertions
+                        )
+                        dfg = st.session_state.tmp_dfg
+                        node = dfg[node_id]
+                        dfg = dfg.reduce_phases_to_below_target(
+                            node.id, Phase.assertions_code
+                        )
+                        st.session_state.tmp_dfg = dfg
+
+                # st.write(f"### Output Checks for {node.pill}")
+                st.session_state.tmp_assertions = [
+                    x for x in st.session_state.tmp_assertions if x
+                ]
+                editable_df = st.data_editor(
+                    pd.DataFrame(
+                        {"checks": st.session_state.tmp_assertions}, dtype=str
+                    ),
+                    key="edit_checks",
+                    num_rows="dynamic",
+                    use_container_width=True,
+                )
+
+                new_assertions = list([x for x in editable_df["checks"] if x])
+                st.session_state.tmp_assertions = new_assertions
+                dfg = st.session_state.tmp_dfg
+                node = dfg[node_id]
+                if (
+                    new_assertions != node.assertions
+                    or node.phase < Phase.assertions_code
+                ):
+                    dfg = dfg.with_node(node.update(assertions=new_assertions))
+                    dfg = dfg.reduce_phases_to_below_target(
+                        node.id, Phase.assertions_code
+                    )
+                    st.session_state.tmp_dfg = dfg
+
+                with buttons.container():
+                    c1, c2, c3 = st.columns(3)
+                    with c1:
+                        if st.button("Save", icon=":material/save:"):
+                            ui_page: UIPage = st.session_state.ui_page
+                            ui_page.page().update_dfg(st.session_state.tmp_dfg)
+                            st.session_state.force_update = True
+                            st.rerun(scope="app")
+
+                    with c2:
+                        st.button(
+                            "Suggest",
+                            icon=":material/format_list_bulleted:",
+                            on_click=make_suggestions,
+                        )
+
+                    with c3:
+                        if show_code():
+                            if st.button("Regenerate"):
+                                with st.spinner("Regenerating..."):
+                                    dfg = st.session_state.tmp_dfg
+                                    dfg = dfg.reduce_phases_to_below_target(
+                                        node_id, Phase.assertions_code
+                                    )
+                                    node = dfg[node_id]
+                                    dfg = dfg.with_node(
+                                        node.update(
+                                            cache=node.cache.invalidate(
+                                                Phase.assertions_code
+                                            )
+                                        )
+                                    )
+                                    st.session_state.tmp_dfg = dfg
+
+                dfg = st.session_state.tmp_dfg
+
+                if dfg[node_id].phase < Phase.assertions_code:
+                    with st.spinner("Generating validation steps..."):
+                        ui_page: UIPage = st.session_state.ui_page
+                        build_config = ui_page.page().base_build_config(repair=False)
+                        engine = BuildEngine.get_builder()
+                        for build_updated in engine.build_with_worklist(
+                            build_config, dfg, Phase.assertions_code, node_id
+                        ):
+                            dfg = build_updated.new_graph
+                        st.session_state.tmp_dfg = dfg
+
+                node = st.session_state.tmp_dfg[node_id]
+                if node.assertion_checks:
+                    for assertion in node.assertions or []:
+                        check = node.assertion_checks.get(assertion, None)
+                        if check and (check.warning or show_code()):
+                            st.divider()
+                            st.write(f"**{assertion}**")
+                            if check.warning:
+                                st.warning(check.warning)
+
+                            if show_code():
+                                if check.type == "quantitative":
+                                    code = check.code
+                                    if code:
+                                        st.code(
+                                            textwrap.indent("\n".join(code), "    ")
+                                        )
+                                    else:
+                                        st.write("    *Code not available*")
+                                else:
+                                    st.write(f"    *{check.requirement}*")
+                else:
+                    st.write("*No details available*")
+            except AssistantError as e:
+                top.error(e)
+
+        for_real(node_id)
+
+    def edit_node(self, node_id: str):
+        ui_page: UIPage = st.session_state.ui_page
+        with ui_page.page():
+            st.session_state.tmp_dfg = ui_page.dfg()
+            st.session_state.tmp_assertions = ui_page.dfg()[node_id].assertions or []
+            self.edit_checks(node_id)
+
+    def show_node_details(self, node: Node):
+        with st.container(key="node_checks", border=True):
+            st.write("###### Checks")
+            assertions = node.assertions or []
+            if assertions:
+                st.write("\n".join(["* " + x for x in assertions]))
+            else:
+                st.write("*Edit node to add checks!*")
+
+        super().show_node_details(node)
+
+    def node_parts_for_diagram(self):
+        keys = [
+            "pill",
+            "messages",
+            "assertions",
+            "requirements",
+            "function_return_type",
+        ]
+        if show_code():
+            keys += ["code"]
+        return keys
+
+    def global_sidebar(self):
+        ui_page: UIPage = st.session_state.ui_page
+
+        st.session_state.node_with_fix_button = None
+
+        failed_assertions = [
+            node.pill
+            for node in ui_page.dfg().nodes
+            if node.filter_messages(Phase.assertions_checked, "error")
+        ]
+        if failed_assertions:
+            items = "\n".join([f"* {x}" for x in failed_assertions])
+            st.error(f"**These nodes have failing checks:**\n{items}")
+
+        warnings = [
+            node.pill
+            for node in ui_page.dfg().nodes
+            if node.filter_messages(Phase.assertions_code, "warning")
+        ]
+        if warnings:
+            items = "\n".join([f"* {x}" for x in warnings])
+            st.warning(f"**These nodes have check warnings:**\n{items}")
+
+        nodes_with_no_assertions = [
+            node.pill for node in ui_page.dfg().nodes if not node.assertions
+        ]
+        nodes_with_no_assertions.sort()
+
+        if nodes_with_no_assertions:
+            items = "\n".join([f"* {x}" for x in nodes_with_no_assertions])
+            st.info(f"**These nodes have no checks:**\n{items}")
+
+            suggest_button = self.suggest_button()
+            if st.button(
+                suggest_button.label,
+                icon=suggest_button.icon,
+                disabled=st.session_state.ama_responding
+                or st.session_state.builder is not None,
+                help=("Suggest tests for nodes that have none. "),
+            ):
+                if st.session_state.builder is None:
+                    st.session_state.builder = Builder(
+                        ui_page.page(),
+                        [x.id for x in ui_page.dfg().nodes if not x.assertions],
+                        target_phase=Phase.assertions_code,
+                        passes_key="suggest-assertions-passes",
+                        force=True,
+                        repair=False,
+                    )
+                    st.session_state.builder_progress = 0
+
+        self.help_details()
+
+    def filter_messages(self, node: Node) -> List[NodeMessage]:
+        return [
+            x
+            for x in node.messages
+            if x.phase <= Phase.assertions_checked and x.level == "error"
+        ]
+
+    def clear_builder_and_reset_state(self):
+        super().clear_builder_and_reset_state()
+        st.session_state.node_id_with_fix_button = None
---
+++ src/flowco/ui/page_files/v2/projects_page.py
@@ -0,0 +1,214 @@
+from ast import Global
+from io import StringIO
+from flowco.dataflow.dfg import Node, dataflow_graph_to_nb
+from flowco.page.page import Page
+from flowco.page.tables import GlobalTables
+from flowco.session.session_file_system import (
+    fs_copy,
+    fs_exists,
+    fs_glob,
+    fs_rm,
+    fs_write,
+)
+
+import streamlit as st
+
+from flowco import __main__
+from flowco.ui.page_files.v2.base_page import FlowcoPage
+from flowco.ui.ui_dialogs import confirm
+from flowco.ui.ui_page import set_ui_page
+from flowco.ui.ui_page import UIPage
+from flowco.util.files import create_zip_in_memory
+
+
+class ProjectsPage(FlowcoPage):
+
+    def button_bar(self):
+        pass
+
+    def graph_is_editable(self) -> bool:
+        return False
+
+    @st.dialog("New project", width="large")
+    def new_project(self):
+        name = st.text_input("Name", placeholder="Project name")
+        if name in self.get_project_names():
+            st.error("Project already exists.")
+        if name.endswith(".flowco"):
+            name = name[: -len(".flowco")]
+        if name and name not in self.get_project_names():
+            st.session_state.just_created_project = True
+            self.add_project(name)
+            st.rerun()
+
+    @st.dialog("New project", width="large")
+    def dup_project(self):
+        name = st.text_input("Name", placeholder="Project name")
+        if name.endswith(".flowco"):
+            name = name[: -len(".flowco")]
+        if name in self.get_project_names():
+            st.error("Project already exists.")
+        if st.button("OK") and name and name not in self.get_project_names():
+            fs_copy(st.session_state.ui_page.page().file_name, f"{name}.flowco")
+            set_ui_page(UIPage(f"{name}.flowco"))
+            st.session_state.selected_node = "<<<<<"
+            st.session_state.clear_graph = True
+            st.session_state.force_update = True
+            st.rerun()
+
+    def select(self):
+        if st.session_state.project_name == ":material/add:":
+            self.new_project()
+        elif st.session_state.project_name == ":material/upload:":
+            self.upload_file()
+        elif st.session_state.project_name is not None:
+            project_name = st.session_state.project_name + ".flowco"
+            print("Loading project", project_name)
+            set_ui_page(UIPage(project_name))
+            st.session_state.selected_node = "<<<<<"
+            st.session_state.clear_graph = True
+            st.session_state.force_update = True
+        else:
+            st.session_state.project_name = self.get_current_project_name()
+        # st.rerun()
+
+    def delete_project(self):
+        file = st.session_state.ui_page.page().file_name
+        fs_rm(file)
+        set_ui_page(None)
+        st.session_state.selected_node = None
+        st.session_state.force_update = True
+        st.session_state.clear_graph = True
+        st.rerun()
+
+    def reset_project(self):
+        st.session_state.ui_page.page().reset()
+        st.session_state.force_update = True
+        st.rerun()
+
+    def add_project(self, name: str):
+        if not name.endswith(".flowco"):
+            name = f"{name}.flowco"
+        Page.create(name)
+        set_ui_page(UIPage(name))
+        st.session_state.selected_node = "<<<<<"
+        st.session_state.force_update = True
+        st.session_state.clear_graph = True
+        # st.rerun()
+
+    def sidebar(self, node: Node | None = None):
+        names = self.get_project_names()
+        current = self.get_current_project_name()
+
+        st.write("# Projects")
+        selected = st.pills(
+            "Select a project",
+            names + [":material/add:", ":material/upload:"],
+            key="project_name",
+            default=current,
+            on_change=self.select,
+            selection_mode="single",
+            label_visibility="collapsed",
+        )
+
+        if current is not None:
+            st.write(f"# {current}.flowco")
+
+            st.info("Select **Edit** above to modify or run the diagram.")
+
+            with st.container(key="page_controls"):
+                cols = st.columns(4)
+                with cols[0]:
+                    st.button(
+                        label="",
+                        icon=":material/clear_all:",
+                        help="Reset all generated content from each node",
+                        on_click=lambda: confirm(
+                            f"Are you sure you want to reset {current}?  This will clear all generated content from each node.",
+                            self.reset_project,
+                        ),
+                    )
+                with cols[1]:
+                    st.button(
+                        label="",
+                        icon=":material/file_copy:",
+                        help=f"Duplicate project",
+                        on_click=self.dup_project,
+                    )
+                with cols[2]:
+                    st.button(
+                        label="",
+                        icon=":material/download:",
+                        help=f"Download project",
+                        on_click=self.download_files,
+                    )
+                with cols[3]:
+                    st.button(
+                        label="",
+                        icon=":material/delete:",
+                        help="Delete project",
+                        on_click=lambda: confirm(
+                            f"Are you sure you want to delete {current}?",
+                            self.delete_project,
+                        ),
+                        disabled=len(self.get_project_names()) <= 1,
+                    )
+
+            ui_page = st.session_state.ui_page
+            # st.write(ui_page.dfg().description)
+
+            if st.session_state.just_created_project:
+                st.session_state.just_created_project = False
+
+    def get_project_names(self):
+        flowco_files = fs_glob("", "*.flowco")
+        names = [file[: -len(".flowco")] for file in flowco_files]
+        return names
+
+    def get_current_project_name(self):
+        return (
+            st.session_state.ui_page.page().file_name[: -len(".flowco")]
+            if st.session_state.ui_page
+            else None
+        )
+
+    @st.dialog("Download Files", width="large")
+    def download_files(self):
+        ui_page = st.session_state.ui_page
+        flowco_name = ui_page.page().file_name
+        tables = GlobalTables.from_dfg(ui_page.dfg())
+        data_files = [x for x in tables.all_files() if x.endswith(".csv")]
+
+        with st.spinner("Creating ZIP file..."):
+            zip_data = create_zip_in_memory(
+                [flowco_name] + data_files,
+                additional_entries={"nb.ipynb": dataflow_graph_to_nb(ui_page.dfg())},
+            )
+
+        st.write("ZIP ready for download!")
+
+        if st.download_button(
+            label=":material/download:",
+            data=zip_data,
+            file_name=f"flowco_files.zip",
+            help="Download the project",
+        ):
+            st.rerun()
+
+    @st.dialog("Upload Project", width="large")
+    def upload_file(self):
+        uploaded_file = st.file_uploader(
+            "Choose a file", type="flowco", accept_multiple_files=False
+        )
+        if uploaded_file is not None:
+            name = uploaded_file.name
+            contents = StringIO(uploaded_file.getvalue().decode("utf-8")).read()
+            if fs_exists(name):
+                st.error("Project already exists.")
+            else:
+                fs_write(name, contents)
+                set_ui_page(UIPage(name))
+                st.session_state.selected_node = "<<<<<"
+                st.session_state.force_update = True
+                st.session_state.clear_graph = True
+                st.rerun()  # hack -- this is in a callback, so technically a no op.
